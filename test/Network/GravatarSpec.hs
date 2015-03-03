b180743eedfa66ee81becc209d32202c029194ef
patrick brisbin <pbrisbin@gmail.com>
2015-03-03 10:58:11 -0500

    Extend spec
    - Test whitespace/case handling
    - Test options usage
    - Refactor repeated, static values

--- test/Network/GravatarSpec.hs
+++ test/Network/GravatarSpec.hs
@@ -2,17 +2,40 @@
 module Network.GravatarSpec where
 
 import Network.Gravatar
-
-import Data.Monoid ((<>))
 import Test.Hspec
+import Data.Monoid ((<>))
+
+import qualified Data.Text as T
 
 main :: IO ()
 main = hspec spec
 
 spec :: Spec
-spec = describe "gravatar" $
-    it "generates the correct URL by hashing the given email" $ do
-        let hsh = "2be502055b6c21ff470730beead2a998"
-            url = "https://www.gravatar.com/avatar/" <> hsh
+spec = describe "gravatar" $ do
+    let email = "pbrisbin@gmail.com"
+        md5sum = "2be502055b6c21ff470730beead2a998"
 
-        gravatar def "pbrisbin@gmail.com" `shouldBe` url
+    it "generates the correct URL by hashing the given email" $
+        gravatar def email `shouldBe`
+            "https://www.gravatar.com/avatar/" <> md5sum
+
+    it "works regardless of whitespace" $
+        gravatar def (" " <> email <> "   ") `shouldBe`
+            "https://www.gravatar.com/avatar/" <> md5sum
+
+    it "works regardless of case" $
+        gravatar def (T.toUpper email) `shouldBe`
+            "https://www.gravatar.com/avatar/" <> md5sum
+
+    it "can be configured via GravatarOptions" $ do
+        let opts = GravatarOptions
+                { gSize = Just $ Size 24
+                , gDefault = Just NotFound
+                , gForceDefault = ForceDefault True
+                , gRating = Just G
+                , gScheme = Http
+                }
+            query = "?s=24&d=404&f=y&r=g"
+
+        gravatar opts email `shouldBe`
+            "http://www.gravatar.com/avatar/" <> md5sum <> query
